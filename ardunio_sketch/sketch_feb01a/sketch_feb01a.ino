3b066cde9bf5af70b9e2085e08aa5a7ce06d0d53
Ankur Khandelwal <ankurkhandelwal08027@gmail.com>
2016-04-19 23:52:27 +0530

    adding serialport to work

--- ardunio_sketch/sketch_feb01a/sketch_feb01a.ino
+++ ardunio_sketch/sketch_feb01a/sketch_feb01a.ino
@@ -1,8 +1,17 @@
+#include <PrintCascade.h>
+#include <SerialDebug.h>
 #include<SPI.h>
 const int ledPin = 13; // the pin that the LED is attached to
+#define debug true;
 void setup() {  
   //initialize serial commm. Serial is global
   Serial.begin(9600);
+
+if(debug){
+  SerialDebugger.begin(9600);
+  SerialDebugger.enable(Error);
+  SerialDebugger.enable(NOTIFICATION);
+  }
 
   // initialize the ledPin as an output:
   pinMode(ledPin,OUTPUT);
@@ -11,11 +20,14 @@ void setup() {
 
 void loop() {  
 //   check if data has been sent from the computer:
-  if(Serial.available()){
+  if(Serial.available() > 0){
     // read the most recent byte (which will be from 0 to 255):
-    int brightness = Serial.parseInt();
-    Serial.print(brightness);
-    digitalWrite(ledPin,1);
-    
-  }  
+    digitalWrite(ledPin,1); 
+    String request=Serial.readString();
+    Serial.println(request);
+    delay(20000);
+    digitalWrite(ledPin,0); 
+      
+  } 
 }
+
