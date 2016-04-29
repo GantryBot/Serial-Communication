9eac42d669d88eb3f4c6318800c6b7a5e54c4e30
Praveen <prrraveen@gmail.com>
2016-04-29 20:12:35 +0530

    cleaned ardunio sketch

--- ardunio_sketch/sketch_feb01a/sketch_feb01a.ino
+++ ardunio_sketch/sketch_feb01a/sketch_feb01a.ino
@@ -1,18 +1,6 @@
-#include <PrintCascade.h>
-#include <SerialDebug.h>
-#include<SPI.h>
 const int ledPin = 13; // the pin that the LED is attached to
-#define debug true;
 void setup() {  
-  //initialize serial commm. Serial is global
   Serial.begin(9600);
-
-if(debug){
-  SerialDebugger.begin(9600);
-  SerialDebugger.enable(Error);
-  SerialDebugger.enable(NOTIFICATION);
-  }
-
   // initialize the ledPin as an output:
   pinMode(ledPin,OUTPUT);
   digitalWrite(ledPin, 0);
@@ -23,11 +11,6 @@ void loop() {
   if(Serial.available() > 0){
     // read the most recent byte (which will be from 0 to 255):
     digitalWrite(ledPin,1); 
-    String request=Serial.readString();
-    Serial.println(request);
-    delay(20000);
-    digitalWrite(ledPin,0); 
-      
   } 
 }
 
